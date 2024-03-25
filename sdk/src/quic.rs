b1f1d7b4e2a0cc790d482b4df3ba23d5679c6057
Smoosh195 <84778934+Smoosh195@users.noreply.github.com>
2024-03-25 15:23:12 +0000

    Update quic.rs

--- sdk/src/quic.rs
+++ sdk/src/quic.rs
@@ -6,7 +6,7 @@ pub const QUIC_PORT_OFFSET: u16 = 6;
 // Empirically found max number of concurrent streams
 // that seems to maximize TPS on GCE (higher values don't seem to
 // give significant improvement or seem to impact stability)
-pub const QUIC_MAX_UNSTAKED_CONCURRENT_STREAMS: usize = 128;
+pub const QUIC_MAX_UNSTAKED_CONCURRENT_STREAMS: usize = 512;
 pub const QUIC_MIN_STAKED_CONCURRENT_STREAMS: usize = 128;
 
 pub const QUIC_TOTAL_STAKED_CONCURRENT_STREAMS: usize = 100_000;
@@ -17,7 +17,7 @@ pub const QUIC_TOTAL_STAKED_CONCURRENT_STREAMS: usize = 100_000;
 // forwarded packets from staked nodes.
 pub const QUIC_MAX_STAKED_CONCURRENT_STREAMS: usize = 512;
 
-pub const QUIC_MAX_TIMEOUT: Duration = Duration::from_secs(2);
+pub const QUIC_MAX_TIMEOUT: Duration = Duration::from_secs(5);
 pub const QUIC_KEEP_ALIVE: Duration = Duration::from_secs(1);
 
 // Based on commonly-used handshake timeouts for various TCP
